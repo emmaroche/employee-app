176b60fac0731d974b0c8614f83d53c4cc3f96e6
emmaroche <20088680@mail.wit.ie>
2022-10-07 13:50:07 +0100

    Improve UI of payslip and employee list

--- src/main/kotlin/ie/setu/Main.kt
+++ src/main/kotlin/ie/setu/Main.kt
@@ -10,18 +10,13 @@ import ie.setu.utils.ScannerInput.readNextInt
 val logger = KotlinLogging.logger {}
 var employee =  Employee("Joe", "Soap", 'm', 6143, 67543.21, 38.5, 5.2, 1450.50, 54.33)
 var employees = EmployeeAPI()
-
-
-
 fun main(args: Array<String>){
     logger.info { "Launching Employee App\n" }
     start()
-
 }
 fun menu() : Int {
-    // Displays the called colour
-    val backgroundGreen = "\u001b[42m"
-    val  backgroundBlue = "\u001b[44m"
+    // Displays the colour
+    val backgroundBlue = "\u001b[44m"
     val black = "\u001b[30m"
     val bold = "\u001b[1m"
     val backgroundBrightGreen = "\u001b[42;1m"
@@ -67,23 +62,23 @@ fun start() {
     } while (input != 0)
 }
 fun add(){
-    print("Enter First name: ")
+    logger.info { "\nYou are adding a new Employee to the list\n" }
+    print("\n" )
+    print("   Enter First name: ")
     val firstName = readLine().toString()
-    print("Enter Surname: ")
+    print("   Enter Surname: ")
     val lastName = readLine().toString()
-    print("Enter Gender (m/f): ")
+    print("   Enter Gender (m/f): ")
     val gender = readLine()!!.toCharArray()[0]
-    print("Enter Employee ID: ")
-    val employeeId = readLine()!!.toInt()
-    print("Enter Gross Salary: ")
+    print("   Enter Gross Salary: ")
     val grossSalary = readLine()!!.toDouble()
-    print("Enter PAYE %: ")
+    print("   Enter PAYE %: ")
     val payePercentage = readLine()!!.toDouble()
-    print("Enter PRSI %: ")
+    print("   Enter PRSI %: ")
     val prsiPercentage = readLine()!!.toDouble()
-    print("Enter Annual Bonus: ")
+    print("   Enter Annual Bonus: ")
     val annualBonus= readLine()!!.toDouble()
-    print("Enter Cycle to Work Deduction: ")
+    print("   Enter Cycle to Work Deduction: ")
     val ctwS= readLine()!!.toDouble()
 
     employees.create(Employee(firstName, lastName, gender, 0, grossSalary, payePercentage, prsiPercentage, annualBonus, ctwS))
@@ -131,7 +126,6 @@ fun updateEmployee() {
         }
     }
 }
-
 fun deleteEmployee() {
     logger.info { "deleteEmployee() function invoked\n" }
     list()
@@ -141,20 +135,31 @@ fun deleteEmployee() {
         // pass the index of the note to EmployeeAPI for deleting and check for success.
         val employeeToDelete = employees.deleteEmployee(indexToDelete)
         if (employeeToDelete != null) {
-            println("Delete Successful! Deleted Employee: ${employeeToDelete.firstName}")
+            logger.info{"Delete Successful! Deleted Employee: ${employeeToDelete.firstName}"}
         } else {
             logger.info{"Delete NOT Successful"}
         }
     }
 }
 fun list(){
-    employees.findAll()
-        .forEach{ println(it) }
+    val backgroundBlue = "\u001b[44m"
+    val black = "\u001b[30m"
+    val bold = "\u001b[1m"
+    val reset = "\u001b[0m"
+    logger.info { "Listing All Employees\n" }
+    print("\n   $backgroundBlue$black$bold Employee List $reset\n")
+        employees.findAll()
+            .forEach{ println(it) }
+
 }
 fun sort(){
-    employees.findAll()
-        .forEach{ println(it) }
-}
+    if (employee != null) {
+        logger.info { "No employee found\n" }
+    }
+    else{
+            employees.findAll()
+                .forEach{ println(it) }}
+    }
 fun search() {
     val employee = getEmployeeById()
     if (employee == null)
@@ -163,7 +168,7 @@ fun search() {
         println(employee)
 }
 internal fun getEmployeeById(): Employee? {
-    print("Enter the employee id to search by: ")
+    print("   Enter the employee id to search by: ")
     val employeeID = readLine()!!.toInt()
     return employees.findOne(employeeID)
 }
@@ -171,6 +176,8 @@ fun paySlip(){
     val employee = getEmployeeById()
     if (employee != null)
         println(employee.getEmployeeInfo())
+    else
+        logger.info{"No employee found linked to that ID\n"}
 }
 fun dummyData() {
     employees.create(Employee("Cian", "Burns", 'M', 1, 35655.43, 31.0, 7.5, 2000.0, 25.6))
--- src/main/kotlin/ie/setu/models/Employee.kt
+++ src/main/kotlin/ie/setu/models/Employee.kt
@@ -1,6 +1,7 @@
 package ie.setu.models
 
 import ie.setu.employee
+import ie.setu.logger
 import ie.setu.roundToTwoDecimalPlaces
 
 class Employee(var firstName: String, var lastName: String, var gender: Char, var employeeId: Int,
@@ -19,40 +20,50 @@ class Employee(var firstName: String, var lastName: String, var gender: Char, va
     fun getTotalDeductions() = roundToTwoDecimalPlaces(getMonthlyPrsi() + getMonthlyPaye() + employee.ctwS)
     fun getMonthlyNetPay() = roundToTwoDecimalPlaces(roundToTwoDecimalPlaces(getGrossPay() - getTotalDeductions()))
     fun getEmployeeInfo() : String {
-
+        val blue = "\u001b[34m"
+        val black = "\u001b[30m"
+        val backgroundBlue = "\u001b[44m"
+        val bold = "\u001b[1m"
+        val reset = "\u001b[0m"
         return """
-     |==================================================================|
-     |                        Monthly Payslip                           |
-     |==================================================================|
-     |                                                                  |
-     |  Employee Name:  ${getFullEmployeeName()}            Employee ID: ${employee.employeeId}     |
-     |                                                                  |
-     |==================================================================|
-     |                                                                  | 
-     | Payment Details:                                                 |
-     |                                                                  |
-     | Gross: ${getGrossPay()}                                                   |
-     | Salary: ${getMonthlySalary()}                                                   |           
-     | Bonus:  ${roundToTwoDecimalPlaces(employee.annualBonus / 12)}                                                   |
-     |                                                                  |
-     |==================================================================|                                                                  
-     |                                                                  | 
-     | Deduction Details:                                               |
-     |                                                                  |
-     | Total Deductions: ${getTotalDeductions()}                                        |
-     | PAYE: ${getMonthlyPaye()}                                                    |     
-     | PRSI: ${getMonthlyPrsi()}                                                     |
-     | Cycle to work: ${employee.ctwS}                                             |
-     |                                                                  | 
-     |==================================================================|
-     | Net pay:  ${getMonthlyNetPay()}                                                |
-     |==================================================================|
+     $blue|==================================================================|$reset
+     $blue|$reset                        $blue$bold Monthly Payslip $reset                         $blue|$reset
+     $blue|==================================================================|$reset 
+     $blue|$reset                                                                  $blue|$reset 
+     $blue|$reset  $backgroundBlue$black Employee Name: $reset  ${getFullEmployeeName()}        $backgroundBlue$black Employee ID: $reset ${employeeId}        $blue|$reset 
+     $blue|$reset                                                                  $blue|$reset 
+     $blue|==================================================================|$reset 
+     $blue|$reset                                                                  $blue|$reset  
+     $blue|$reset  $backgroundBlue$black Payment Details: $reset                                              $blue|$reset 
+     $blue|$reset                                                                  $blue|$reset 
+     $blue|  Monthly Salary:$reset ${getGrossPay()}                                         $blue|$reset            
+     $blue|  Bonus:$reset ${roundToTwoDecimalPlaces(employee.annualBonus / 12)}                                                   $blue|$reset 
+     $blue|$reset                                                                  $blue|$reset 
+     $blue|==================================================================|$reset                                                                   
+     $blue|$reset                                                                  $blue|$reset  
+     $blue|$reset  $backgroundBlue$black Deduction Details: $reset                                            $blue|$reset 
+     $blue|$reset                                                                  $blue|$reset 
+     $blue|  Total Deductions:$reset ${getTotalDeductions()}                                       $blue|$reset 
+     $blue|  PAYE:$reset ${getMonthlyPaye()}                                                   $blue|$reset      
+     $blue|  PRSI:$reset ${getMonthlyPrsi()}                                                    $blue|$reset 
+     $blue|  Cycle to work:$reset ${employee.ctwS}                                            $blue|$reset 
+     $blue|$reset                                                                  $blue|$reset 
+     $blue|==================================================================|$reset 
+     $blue|$reset                                                                  $blue|$reset  
+     $blue|$reset  $backgroundBlue$black Net pay: $reset ${getMonthlyNetPay()}                                              $blue|$reset 
+     $blue|$reset                                                                  $blue|$reset  
+     $blue|==================================================================|$reset 
      """
     }
 
     override fun toString(): String {
-        return "\nEmployee ID: $employeeId \tName: $firstName $lastName ($gender)" +
-                "\n--------------------------------------" + " \nGross Salary: $grossSalary | PAYE Percentage: $payePercentage | PRSI Percentage: $prsiPercentage | Annual Bonus: $annualBonus | Cycle To Work MonthlyDeduction: $ctwS \n "
+        val blue = "\u001b[34m"
+        val bold = "\u001b[1m"
+        val reset = "\u001b[0m"
+        return "\n  $blue$bold Employee ID: $reset $employeeId \t $blue$bold Name: $reset  $firstName $lastName ($gender)" +
+                "\n   -----------------------------------------" + " \n  $blue Gross Salary: $reset $grossSalary " +
+                "|$blue$bold PAYE Percentage: $reset $payePercentage |$blue$bold PRSI Percentage: $reset $prsiPercentage " +
+                "|$blue$bold Annual Bonus: $reset $annualBonus |$blue$bold Cycle To Work MonthlyDeduction: $reset $ctwS \n "
     }
 }
 
